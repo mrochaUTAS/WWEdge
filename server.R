1c143c780197711b098924770f2091989b93dae0
mrochaUTAS <72726521+mrochaUTAS@users.noreply.github.com>
2020-12-12 20:41:32 +1100

    Add files via upload

--- server.R
+++ server.R
@@ -1,18 +1,9 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+###
+###
 
 library(shiny)
 library(shinyFiles)
-library(shinyDirectoryInput)
 library(fs)
-library(knitr)
-library(rmarkdown)
 
 server <-  function(input, output, session) {
 
@@ -78,10 +69,8 @@ server <-  function(input, output, session) {
       )
     }
   )
-  
 
     })
-  
   
   }
   
--- ui.R
+++ ui.R
@@ -4,9 +4,7 @@
 
 library(shiny)
 library(shinyFiles)
-library(shinyDirectoryInput)
 library(fs)
-library(knitr)
 
 ui = fluidPage(
   sliderInput("slider", "Slider", 1, 100, 50),
@@ -17,12 +15,8 @@ ui = fluidPage(
   fileInput("myFileD", "Upload dried wedge images", accept = c('image/png', 'image/jpeg'), multiple = TRUE),
 
   shinyDirButton("directoryG", "Green wedges", "Please select a folder of green wedges"),
-  tags$p(),
-  tags$p(),
   
   shinyDirButton("directoryD", "Dried wedges", "Please select a folder of dried wedges"),
-  tags$p(),
-  tags$p(),
   
   downloadButton("report", "Generate report"),
   
